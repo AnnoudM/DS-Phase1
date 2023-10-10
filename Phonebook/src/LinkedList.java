c26e95fc8647f57d2231b7ae0f960acd1ba4d39f
Leena2S <145167830+Leena2S@users.noreply.github.com>
2023-10-10 17:46:14 +0300

    Update LinkedList.java

--- Phonebook/src/LinkedList.java
+++ Phonebook/src/LinkedList.java
@@ -1,4 +1,142 @@
 
-public class LinkedList {
+	public class LinkedList <T extends Comparable<T>>{
+	    private Node<T> head;
+	    private Node<T> current;
+	    int size;
+	    
+	    public LinkedList () {
+	        head = current = null;
+	        size = 0;
+	    }
+	    
+	    public boolean empty () {
+	        return head == null;
+	    }
+	    
+	    public boolean last () {
+	        return current.next == null;
+	    }
+	    
+	    public boolean full () {
+	            return false;
+	    }
+	    
+	    public void findFirst () {
+	            current = head;
+	    }
+	    public void findNext () {
+	            current = current.next;
+	    }
+	    public T retrieve () {
+	            return current.data;
+	    }
+	    public void update (T val) {
+	            current.data = val;
+	    }
+	    
+	    public void insertAtEnd (T val) {
+	            if (empty()) {
+	                    current = head = new Node<T> (val);
+	            }
+	            else {
+	                    while (current.next != null )
+	                        current = current.next;
+	                    
+	                    current.next = new Node<T> (val);
+	                    current = current.next;
+	            }
+	            size++;
+	    }
 
-}
+	    public boolean insert(T val) {
+	           
+	            Node<T> tmp;
+	            if (empty()) {
+	                    current = head = new Node<T> (val);
+	            }
+	            else {
+	                if ( head.getData().compareTo(val) >0)
+	                {
+	                    tmp = new Node<T>(val);
+	                    tmp.setNext(head);
+	                    head = tmp;
+	                }
+	                else
+	                {
+	                    Node<T> prev = null;
+	                    current = head;
+	                    
+	                    while (( current != null ) && (current.getData().compareTo(val) <= 0))
+	                    {
+	                        prev = current;
+	                        current = current.getNext();
+	                    }
+	                    tmp = new Node<T> (val);
+	                    if ( current != null)
+	                    {
+	                        tmp.next = current;
+	                        prev.next = tmp;
+	                        current = tmp;
+	                    }
+	                    else
+	                        current = prev.next =tmp;
+	                }
+	            }
+	            size++;
+	            return true;
+	    }
+
+	    public boolean search (T val)
+	    {
+	        if (head == null)
+	            return false;
+	        
+	        Node<T> node  = head;
+	        while (node != null) { //while(node!= null)
+	        	if  (node.getData().compareTo(val) == 0) {
+	            current = node;
+	            return true;}
+	        	else
+	            node = node.getNext();}
+	        return false;
+	        }
+	       
+	   
+	            
+	    public T remove (T val) {
+	            
+	        if (search (val) == false)
+	         return null;
+
+	        T data = current.getData();
+	        
+	        if (current == head) {
+	                head = head.next;
+	        }
+	        else {
+	                Node <T> tmp = head;
+
+	                while (tmp.next != current)
+	                        tmp = tmp.next;
+	               tmp.next = current.next;
+	        }
+	        if (current.next == null)
+	                current = head;
+	        else
+	                current = current.next;
+	        size -- ;
+	        return data;    
+	    }
+
+	        public void printAll ()
+	        {
+	            Node <T> p = head;
+	            while ( p != null)
+	            {
+	                System.out.print(p.data + "    ");
+	                p = p .next;
+	            }
+	            System.out.println("");
+	        }
+	}
+
